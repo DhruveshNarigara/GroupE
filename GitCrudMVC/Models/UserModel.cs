239754612957b3769ae26df6b64ed32bf3a153dc
dar123-art <darshit2817@gmail.com>
2024-02-22 16:03:33 +0530

    model

--- GitCrudMVC/Models/UserModel.cs
+++ GitCrudMVC/Models/UserModel.cs
@@ -7,12 +7,12 @@ namespace GitCrudMVC.Models
 {
     public class UserModel
     {
-        public int c_userid{get ; set ;}
+        // public int c_userid{get ; set ;}
 
-        public string c_username { get ; set ;}
+        // public string c_username { get ; set ;}
 
-        public string c_email { get ; set ;}
+        // public string c_email { get ; set ;}
 
-        public string c_password { get ; set ;}
+        // public string c_password { get ; set ;}
     }
 }
